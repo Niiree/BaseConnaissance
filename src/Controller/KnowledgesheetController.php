bc03382159abbf0ee11d35d13bbbc3ed26406da0
Nicolas <nicolas.le-stunff@hotmail.fr>
2020-01-19 15:04:47 +0100

    Base de la page de Création fiche de connaissance

--- src/Controller/KnowledgesheetController.php
+++ src/Controller/KnowledgesheetController.php
@@ -2,8 +2,10 @@
 
 namespace App\Controller;
 
+use App\Form\KnowledgesheetType;
 use App\Repository\KnowledgesheetRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\FormInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class KnowledgesheetController extends AbstractController
@@ -18,9 +20,12 @@ class KnowledgesheetController extends AbstractController
         ]);
     }
     /**
-     * @Route("/knowledgesheet/create", name="knowledgesheet_create)
-     *
+     * @Route("/knowledgesheet/create", name="knowledgesheet")
      */
-    public create ()
-
+    public function create(KnowledgesheetRepository $knowledgesheetRepository)
+    {
+        return $this->render('knowledgesheet/create.html.twig', [
+            'controller_name' => 'KnowledgesheetController',
+        ]);
+    }
 }
