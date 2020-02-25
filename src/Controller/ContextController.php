ae202297c20aaff0a3daac76a5a3224b74c52457
Nicolas <nicolas.le-stunff@hotmail.fr>
2020-02-25 03:44:14 +0100

    Administration du contexte

--- src/Controller/ContextController.php
+++ src/Controller/ContextController.php
@@ -2,8 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Context;
+use App\Form\ContextType;
 use App\Repository\ContextRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -14,18 +18,36 @@ class ContextController extends AbstractController
      */
     public function index(ContextRepository $contextRepository, Request $request)
     {
-        return $this->render('context/index.html.twig', [
-            'contexts' => $contextRepository->findAll(),
-        ]);
+        return $this->render('context/index.html.twig');
     }
 
     /**
-     * @Route("/admin/context_create", name="context_create")
+     * @Route("/admin/context", name="context_create")
      */
-    public function create(ContextRepository $contextRepository, Request $request)
+    public function create(EntityManagerInterface $entityManager, ContextRepository $contextRepository, Request $request)
     {
-        return $this->render('context/index.html.twig', [
-            'contexts' => $contextRepository->findAll(),
+        $context = new Context();
+
+        $form=$this->createForm(ContextType::class,$context);
+        $form->add('edit',SubmitType::class,
+            ['label'=>'Créer']);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form-> isValid()) {
+            $context = $form->getData();
+            $entityManager->persist($context);
+            $entityManager->flush();
+            return $this->redirectToRoute('context_create');
+        }
+        return $this ->render('context/create.html.twig',[
+            'formContext' => $form->createView(),'contexts' =>$contextRepository->findAll(),
         ]);
+
+
+
+
+
+
+
     }
 }
--- src/Form/ContextType.php
+++ src/Form/ContextType.php
@@ -3,6 +3,7 @@
 namespace App\Form;
 
 use App\Entity\Context;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -12,7 +13,7 @@ class ContextType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name')
+            ->add('name',TextType::class,['label'=>'Nom du contexte'])
         ;
     }
 
