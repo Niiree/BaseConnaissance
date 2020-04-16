eb968010c5e443a9fc504cd31eeae12460706eba
Nicolas Le stunff <58040603+nicolas-le-stunff@users.noreply.github.com>
2020-04-16 17:24:51 +0200

    Update Version20200306155628.php

--- src/Migrations/Version20200306155628.php
+++ src/Migrations/Version20200306155628.php
@@ -22,11 +22,11 @@ final class Version20200306155628 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
 
-        $this->addSql('CREATE TABLE context_users (context_id INT NOT NULL, users_id INT NOT NULL, PRIMARY KEY(context_id, users_id))');
-        $this->addSql('CREATE INDEX IDX_4594C9976B00C1CF ON context_users (context_id)');
+        $this->addSql('CREATE TABLE users_context (context_id INT NOT NULL, users_id INT NOT NULL, PRIMARY KEY(context_id, users_id))');
+        $this->addSql('CREATE INDEX IDX_4594C9976B00C1CF ON users_context (context_id)');
         $this->addSql('CREATE INDEX IDX_4594C99767B3B43D ON context_users (users_id)');
-        $this->addSql('ALTER TABLE context_users ADD CONSTRAINT FK_4594C9976B00C1CF FOREIGN KEY (context_id) REFERENCES context (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
-        $this->addSql('ALTER TABLE context_users ADD CONSTRAINT FK_4594C99767B3B43D FOREIGN KEY (users_id) REFERENCES users (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE users_context ADD CONSTRAINT FK_4594C9976B00C1CF FOREIGN KEY (context_id) REFERENCES context (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE users_context ADD CONSTRAINT FK_4594C99767B3B43D FOREIGN KEY (users_id) REFERENCES users (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
     }
 
     public function down(Schema $schema) : void
@@ -35,6 +35,6 @@ final class Version20200306155628 extends AbstractMigration
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
 
         $this->addSql('CREATE SCHEMA public');
-        $this->addSql('DROP TABLE context_users');
+        $this->addSql('DROP TABLE users_context');
     }
 }
