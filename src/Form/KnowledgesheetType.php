621c5f4d5154bbcca45071da124d9b87946dd82a
Elfatih <elfatih.ibrahim@yahoo.com>
2020-05-07 00:12:33 +0200

    La relation Knowlegdesheets-contetx et mise a jour la page dAccueil

--- src/Form/KnowledgesheetType.php
+++ src/Form/KnowledgesheetType.php
@@ -2,9 +2,11 @@
 
 namespace App\Form;
 
+use App\Entity\Context;
 use App\Entity\Knowledgesheet;
 use Doctrine\DBAL\Types\StringType;
 use FOS\CKEditorBundle\Form\Type\CKEditorType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
@@ -20,6 +22,14 @@ class KnowledgesheetType extends AbstractType
             ->add('content', CKEditorType::class,['label'=>'Contenu'])
             ->add('keyword',TextType::class,['label' =>'Mot-clé'])
         ;
+
+        $builder->add('contexts', EntityType::class, [
+            'class' => Context::class,
+            'choice_label' => 'name',
+            'multiple' => true,
+            'expanded' => true,
+        ]);
+
     }
 
     public function configureOptions(OptionsResolver $resolver)
