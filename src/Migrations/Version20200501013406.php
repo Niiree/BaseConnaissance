08296ebb68fc66b3d29fdd689d9418dd2501df46
Nicolas Le stunff <58040603+nicolas-le-stunff@users.noreply.github.com>
2020-05-07 18:14:10 +0200

    Update Version20200501013406.php

--- src/Migrations/Version20200501013406.php
+++ src/Migrations/Version20200501013406.php
@@ -27,6 +27,7 @@ final class Version20200501013406 extends AbstractMigration
         $this->addSql('CREATE INDEX IDX_9A53F3586B00C1CF ON knowledgesheet_context (context_id)');
         $this->addSql('ALTER TABLE knowledgesheet_context ADD CONSTRAINT FK_9A53F358801DC957 FOREIGN KEY (knowledgesheet_id) REFERENCES knowledgesheet (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
         $this->addSql('ALTER TABLE knowledgesheet_context ADD CONSTRAINT FK_9A53F3586B00C1CF FOREIGN KEY (context_id) REFERENCES context (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('DROP INDEX "primary"');
         $this->addSql('ALTER TABLE users_context ADD PRIMARY KEY (users_id, context_id)');
         $this->addSql('ALTER INDEX idx_4594c99767b3b43d RENAME TO IDX_125DF1B467B3B43D');
         $this->addSql('ALTER INDEX idx_4594c9976b00c1cf RENAME TO IDX_125DF1B46B00C1CF');
