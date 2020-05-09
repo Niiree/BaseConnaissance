87b9c54765e3ee5038641a6bdb6feddbd47ece65
Nicolas <nicolas.le-stunff@hotmail.fr>
2020-05-09 03:45:52 +0200

    Polish sur les fichiers repository

--- src/Repository/ContextRepository.php
+++ src/Repository/ContextRepository.php
@@ -18,33 +18,4 @@ class ContextRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, Context::class);
     }
-
-    // /**
-    //  * @return Context[] Returns an array of Context objects
-    //  */
-    /*
-    public function findByExampleField($value)
-    {
-        return $this->createQueryBuilder('c')
-            ->andWhere('c.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('c.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
-
-    /*
-    public function findOneBySomeField($value): ?Context
-    {
-        return $this->createQueryBuilder('c')
-            ->andWhere('c.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
-    }
-    */
 }
